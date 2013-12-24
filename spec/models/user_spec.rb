3e6c8086461e7025a233a7e9f52120ab3b3cf758
Dan S Turner <turner@nowwithmoredan.com>
2013-12-24 15:16:25 -0800

    Add Sessions controller and sign-in/-out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -100,5 +101,10 @@ describe User do
   describe "when trying to set a password shorter than 6 characters" do
     before { @user.password = @user.password_confirmation = "12345" }
     it { should_not be_valid }
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
